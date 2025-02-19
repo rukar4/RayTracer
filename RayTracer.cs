76fdedd6cc39196f3264d2cdecb8e28ddf2d7828
Reid Merrell <r4merrell@yahoo.com>
2025-02-18 20:40:23 -0700

    feat: ppm writer

--- RayTracer.cs
+++ RayTracer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 public class Vector {
     public float x;
@@ -40,8 +41,63 @@ public class Ray {
     }
 }
 
+public class PPMWriter {
+    private string PPM_TYPE = "P3";
+    private int MAX_COLOR_VAL = 255;
+
+    private StreamWriter writer;
+    private string file;
+    private int height;
+    private int width;
+
+    public PPMWriter(string file, int height, int width) {
+        this.file = file;
+        this.height = height;
+        this.width = width;
+        writer = new StreamWriter(file);
+
+        // Initialize file
+        writer.WriteLine(PPM_TYPE);
+        writer.WriteLine($"{width} {height}");
+        writer.WriteLine($"{MAX_COLOR_VAL}");
+    }
+
+    public void WriteRGB(int r, int g, int b) {
+        writer.Write($"{r} {g} {b}\t");
+    }
+
+    public void WriteLine() {
+        writer.WriteLine();
+    }
+
+    public void Dispose() {
+        writer?.Close();
+        writer?.Dispose();
+    }
+}
+
 class RayTracer {
     static void Main() {
-        
+        int width = 256;
+        int height = 256;
+
+        PPMWriter writer = new PPMWriter("sphere.ppm", height, width);
+
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                double r = (double) i / (width - 1);
+                double g = (double) j / (height - 1);
+                double b = 0.0;
+
+                int ir = (int) (255.999 * r);
+                int ig = (int) (255.999 * g);
+                int ib = (int) (255.999 * b);
+                
+                writer.WriteRGB(ir, ig, ib);
+            }
+            writer.WriteLine();
+        }
+
+        writer.Dispose();
     }
 }
