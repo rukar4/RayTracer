113f650333739364704179a054ad4dce82e494f5
Reid Merrell <r4merrell@yahoo.com>
2025-02-18 19:38:27 -0700

    init: initialize repo

---
+++ RayTracer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Vector {
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void Add(Vector vector) {
+        x += vector.x;
+        y += vector.y;
+        z += vector.z;
+    }
+
+    public void Update(Vector vector) {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+    
+    public Vector GetSum(Vector vector) {
+        return new Vector(x + vector.x, y + vector.y, z + vector.z);
+    }
+}
+
+public class Ray {
+    public Vector origin;
+    public Vector dir;
+    public Vector dest;
+
+    public Ray(float x0, float y0, float z0, float xd, float yd, float zd) {
+        origin = new Vector(x0, y0, z0);
+        dir = new Vector(xd, yd, zd);
+        dest = origin.GetSum(dir);
+    }
+}
+
+class RayTracer {
+    static void Main() {
+        
+    }
+}
